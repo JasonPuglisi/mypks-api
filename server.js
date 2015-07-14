e86cbcb642b811611001115c91a408b764b5ffdf
Jason Puglisi <jason@puglisi.im>
2015-07-14 17:55:49 -0400

    Updated to v0.3.0 including email getting/setting (readme not updated yet)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Declare dependencies
 var express = require('express');
 var moment = require('moment');
+var validator = require('validator');
 var zombie = require('zombie');
 
 // Start web server on port 8080 unless otherwise specified
@@ -19,9 +20,35 @@ app.get('/', function(req, res) {
 	// Set response type to JSON
 	res.setHeader('Content-Type', 'application/json');
 
+	// Attempt to log in and get schedule
+	logIn(res, req.query, 'getSchedule');
+
+});
+
+// Recieve email page request in form: http://127.0.0.1:8080/email?id=00000000&bd=0000
+// * Set email by adding addr parameter: http://127.0.0.1:8080/email?id=00000000&bd=0000&addr=example@example.com
+app.get('/email', function(req, res) {
+
+	// Set response type to JSON
+	res.setHeader('Content-Type', 'application/json');
+
+	// Attempt to login and update email address if new address not specified
+	if (req.query.addr === undefined)
+		logIn(res, req.query, 'getEmail');
+
+	// Attempt to login and get email address
+	else
+		logIn(res, req.query, 'setEmail');
+
+});
+
+// Attempt to log into mypks
+// * A headless browser is used to satisfy hidden fields
+function logIn(res, params, action) {
+
 	// Get username and password from URL
-	var id = req.query.id;
-	var bd = req.query.bd;
+	var id = params.id;
+	var bd = params.bd;
 
 	// Send error if username and password are invalid
 	if (isNaN(id) && isNaN(bd))
@@ -36,57 +63,70 @@ app.get('/', function(req, res) {
 		sendError(res, 'Invalid birthdate format');
 
 	// Proceed to log in attempt
-	else
-		logIn(res, id, bd);
+	else {
 
-});
+		// Create browser and set page URLs
+		var browser = new zombie();
+		var destination = 'http://mypks.com/';
+		var target = 'http://mypks.com/default.aspx';
 
-// Attempt to log into mypks
-// * A headless browser is used to satisfy hidden fields
-function logIn(res, id, bd) {
+		// Visit login page and attempt login with specified credentials
+		browser.visit(destination, function(e) {
+			browser.fill('txtBadge', id).fill('txtBirthDate', bd).pressButton('btnOK', function() {
 
-	// Create browser and set page URLs
-	var browser = new zombie();
-	var destination = 'http://mypks.com/';
-	var target = 'http://mypks.com/default.aspx';
+				// Make sure login form is still present
+				try {
+					browser.assert.element('#txtBadge');
 
-	// Visit login page and attempt login with specified credentials
-	browser.visit(destination, function(document) {
-		browser.fill('txtBadge', id).fill('txtBirthDate', bd).pressButton('btnOK', function() {
+					// Attempt to log in again if target URL not reached
+					// * Even in normal browsers, mypks sometimes only works after two login attempts
+					if (browser.url !== target) {
+						browser.fill('txtBadge', id).fill('txtBirthDate', bd).pressButton('btnOK', function() {
 
-			// Make sure login form is still present
-			try {
-				browser.assert.element('#txtBadge');
+							// Send error if target URL not reached
+							if (browser.url !== target)
+								sendError(res, 'Unable to authenticate');
 
-				// Attempt to log in again if target URL not reached
-				// * Even in normal browsers, mypks sometimes only works after two login attempts
-				if (browser.url !== target) {
-					browser.fill('txtBadge', id).fill('txtBirthDate', bd).pressButton('btnOK', function() {
+							// Proceed to action processing
+							else
+								processAction(res, params, action, browser);
 
-						// Send error if target URL not reached
-						if (browser.url !== target)
-							sendError(res, 'Unable to authenticate');
+						});
+					}
 
-						// Proceed to schedule parsing
-						else
-							parseSchedule(res, browser.document);
-
-					});
+					// Proceed to action processing
+					else
+						processAction(res, params, action, browser);
 				}
 
-				// Proceed to schedule parsing
-				else
-					parseSchedule(res, browser.document);
-			}
+				// Send error if login form is not still present
+				catch (e) {
+					sendError(res, 'Unable to authenticate');
+				}
 
-			// Send error if login form is not still present
-			catch (e) {
-				sendError(res, 'Unable to authenticate');
-			}
-
+			});
 		});
-	});
 
+	}
+
+}
+
+// Process page actions on mypks
+function processAction(res, params, action, browser) {
+	switch (action) {
+
+		// Get schedule from main page
+		case 'getSchedule':
+			parseSchedule(res, browser.document);
+		break;
+
+		// Get or set email from email page
+		case 'getEmail':
+		case 'setEmail':
+			processEmail(res, params, action, browser);
+		break;
+
+	}
 }
 
 // Scrape and parse schedule from mypks
@@ -145,6 +185,56 @@ function parseSchedule(res, document) {
 
 }
 
+// Process and update email from mypks
+function processEmail(res, params, action, browser) {
+
+	// Set email page URL
+	var destination = 'http://mypks.com/emailNotice.aspx';
+
+	// Visit email page and process email action
+	browser.visit(destination, function(e) {
+
+		// Create email address store
+		var email = {};
+
+		// Set old/current email address
+		email.old = browser.document.getElementById('txtEmail').value;
+
+		// Set old/current email address to null if blank
+		if (!email.old)
+			email.old = null;
+
+		switch (action) {
+
+			// Send email address
+			case 'getEmail':
+				sendEmail(res, email.old);
+			break;
+
+			// Set and send email address
+			case 'setEmail':
+
+				// Set new email address
+				email.new = params.addr;
+
+				// Send invalid email address error if email address is not valid
+				if (!validator.isEmail(email.new))
+					sendError(res, 'Invalid email address');
+
+				// Update and send email address
+				else {
+					browser.fill('txtEmail', email.new).pressButton('Button1', function() {
+						sendEmail(res, email);
+					});
+				}
+
+			break;
+
+		}
+	});
+
+}
+
 // Send success response with shifts
 function sendShifts(res, shifts) {
 	res.end(JSON.stringify({
@@ -159,5 +249,13 @@ function sendError(res, e) {
 	res.end(JSON.stringify({
 		'success': false,
 		'error': e
+	}));
+}
+
+// Send success response with email
+function sendEmail(res, email) {
+	res.end(JSON.stringify({
+		'success': true,
+		'email': email
 	}));
 }
